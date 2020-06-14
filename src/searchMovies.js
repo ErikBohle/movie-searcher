a601bae51390180731660684bbc2f673e5dc90e0
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-06-14 05:27:13 +0200

    end

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,29 +1,41 @@
-import React from "react";
-
+import React, {useState} from "react";
+import MovieCard from './movieCard.js';
 export default function SearchMovies(){
+    
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+    //create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
     
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
+                
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=ecdc1a20e77cb3dfb3d2ef57cd839b0b&language=en-US&query=${query}&page=1&include_adult=false`;
         
-        const query = "Jurassic Park";
-        
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         try {
-        const res = await fetch(url);
-        const data  = await res.json();
-        console.log(data);
-    }catch(err){
-        console.log(err)
+            const res = await fetch(url);
+            const data  = await res.json();
+            setMovies(data.results);
+        }catch(err){
+            console.error(err);
+        }
     }
-    };
     
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"/>
-            <button className="button" type="submit">Search</button>
-        </form>
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" type="text" name="query"
+                    placeholder="i.e. Jurassic Park"
+                    value={query} onChange={(e) => setQuery(e.target.value)}
+                    />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                   <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>    
+        </>
     )
 }
